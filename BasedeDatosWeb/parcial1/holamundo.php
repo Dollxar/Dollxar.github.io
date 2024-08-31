32c7a98ce754995036a4a956c8f1474f8987432f
German Eduardo Reyes Escamilla <135774097+Dollxar@users.noreply.github.com>
2024-08-30 18:42:23 -0700

    Practica #1 Hola Mundo en PHP 30/08/2024

---
+++ BasedeDatosWeb/parcial1/holamundo.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = "Luis";
+$numero = 4;
+$numero2 = "4";
+
+$total = $numero + $numero2;
+
+echo "Total = ". $total;
+
+echo "<h1> HOLA MUNDO </h1>";
+
+for ($i=0; $i < 10; $i++) { 
+    echo $i . "<br>";
+}
+echo "<h2 style='background-color: yellow;'> German Eduardo Reyes Escamilla 5AVP </h2>"
+?>  
